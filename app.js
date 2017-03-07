9c138876e2777f73047d30200f8f79f7af9dce40
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-03-07 15:46:24 -0800

    hello

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var userName = prompt('Hello! what is your name?');
+
+alert('Hi, ' + userName + ' It is a pleasure to meet you');
+console.log('Name:' + userName);
+
+var userAnswer1 = confirm('Do you want to get to know me a little better?');
+if (userAnswer1 === true) {
+  alert('sweet ' + userName + 'I knew you were a bro');
+} else {
+  alert('well too bad, you\'re about to learn anyway');
+}
+
+var userAnswer2 = confirm('Do you want to get to know me a little better?');
+if (userAnswer2 === true) {
+  alert('sweet ' + userName + 'I knew you were a bro');
+} else {
+  alert('well too bad, you\'re about to learn anyway');
+}
+
+var userAnswer3 = confirm('Do you want to get to know me a little better?');
+if (userAnswer3 === true) {
+  alert('sweet ' + userName + 'I knew you were a bro');
+} else {
+  alert('well too bad, you\'re about to learn anyway');
+}
+
+var userAnswer4 = confirm('Do you want to get to know me a little better?');
+if (userAnswer4 === true) {
+  alert('sweet ' + userName + 'I knew you were a bro');
+} else {
+  alert('well too bad, you\'re about to learn anyway');
+}
+
+var userAnswer5 = confirm('Do you want to get to know me a little better?');
+if (userAnswer5 === true) {
+  alert('sweet ' + userName + 'I knew you were a bro');
+} else {
+  alert('well too bad, you\'re about to learn anyway');
+}
