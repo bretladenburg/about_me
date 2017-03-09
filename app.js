36b35c46117467673999c2f8f75fa506e5c3135a
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-03-08 17:20:12 -0800

    added a counter to the javascript file

--- app.js
+++ app.js
@@ -1,59 +1,66 @@
 'use strict';
 
-// var userName = prompt('Hello! what is your name?');
-//
-// alert('Hi, ' + userName + ' It is a pleasure to meet you');
-// console.log('Name:' + userName);
-//
-// var userAnswer1 = prompt('do you wanna hang out today?');
-// console.log('hanging out today?:' + userAnswer1);
-// if (userAnswer1.toLowerCase() === 'yes' || userAnswer1.toLowerCase() === 'y') {
-//   alert('wow ' + userName + ' that\'s kind of weird, you barely know me');
-// } else if (userAnswer1.toLowerCase() === 'no' || userAnswer1.toLowerCase() === 'n') {
-//   alert('fine I didn\'t want to hang either!');
-// } else {
-//   alert('Bro, what are you typing?');
-// }
-//
-// var userAnswer2 = prompt('do you wanna get some ice cream?');
-// console.log('we getting ice cream?:' + userAnswer2);
-// if (userAnswer2.toLowerCase() === 'yes' || userAnswer2.toLowerCase() === 'y') {
-//   alert('nice that sounds really yummy right now!');
-// } else if (userAnswer2.toLowerCase() === 'no' || userAnswer2.toLowerCase() === 'n') {
-//   alert('did you want to get something else to eat?');
-// } else {
-//   alert('you sure you got the right answer?');
-// }
-//
-// var userAnswer3 = prompt('do you have an interest in computers?');
-// console.log('Interest in computers?:' + userAnswer3);
-// if (userAnswer3.toLowerCase() === 'yes' || userAnswer3.toLowerCase() === 'y') {
-//   alert('awesome so do I!');
-// } else if (userAnswer3.toLowerCase() === 'no' || userAnswer3.toLowerCase() === 'n') {
-//   alert('you must be afraid of the machines, skynet could happen after all');
-// } else {
-//   alert('your answer does not compute');
-// }
-//
-// var userAnswer4 = prompt('do you like the name your parents gave you?');
-// console.log('Like your name?:' + userAnswer4);
-// if (userAnswer4.toLowerCase() === 'yes' || userAnswer4.toLowerCase() === 'y') {
-//   alert('yeah ' + userName + ' seems like a pretty legit name');
-// } else if (userAnswer4.toLowerCase() === 'no' || userAnswer4.toLowerCase() === 'n') {
-//   alert('I can feel you man.');
-// } else {
-//   alert('are you putting in sensible english?');
-// }
-//
-// var userAnswer5 = prompt('did you like this series of weird questions?');
-// console.log('Like these weird questions?:' + userAnswer5);
-// if (userAnswer5.toLowerCase() === 'yes' || userAnswer5.toLowerCase() === 'y') {
-//   alert('alright thats pretty nice of you.');
-// } else if (userAnswer5.toLowerCase() === 'no' || userAnswer5.toLowerCase() === 'n') {
-//   alert('it\'s ok, I wasn\'t a big fan either..');
-// } else {
-//   alert('Bro, what are you typing?');
-// }
+var counter = 0;
+var message1 = 'sorry, that\'s not a valid response';
+var userName = prompt('Hello! what is your name?');
+
+alert('Hi, ' + userName + ' It is a pleasure to meet you');
+console.log('Name:' + userName);
+
+var userAnswer1 = prompt('do you wanna hang out today?');
+console.log('hanging out today?:' + userAnswer1);
+if (userAnswer1.toLowerCase() === 'yes' || userAnswer1.toLowerCase() === 'y') {
+  alert('wow ' + userName + ' that\'s kind of weird, you barely know me');
+  counter++;
+} else if (userAnswer1.toLowerCase() === 'no' || userAnswer1.toLowerCase() === 'n') {
+  alert('fine I didn\'t want to hang either!');
+} else {
+  alert(message1);
+}
+
+var userAnswer2 = prompt('do I have any siblings?');
+console.log('siblings?:' + userAnswer2);
+if (userAnswer2.toLowerCase() === 'yes' || userAnswer2.toLowerCase() === 'y') {
+  alert('congrats! you are right');
+  counter++;
+} else if (userAnswer2.toLowerCase() === 'no' || userAnswer2.toLowerCase() === 'n') {
+  alert('don\'t worry you\'re just getting to know me');
+} else {
+  alert(message1);
+}
+
+var userAnswer3 = prompt('do you think I have an interest in computers?');
+console.log('Interest in computers?:' + userAnswer3);
+if (userAnswer3.toLowerCase() === 'yes' || userAnswer3.toLowerCase() === 'y') {
+  alert('awesome so do I!');
+  counter++;
+} else if (userAnswer3.toLowerCase() === 'no' || userAnswer3.toLowerCase() === 'n') {
+  alert('you must be think I believe in skynet');
+} else {
+  alert(message1);
+}
+
+var userAnswer4 = prompt('do you think i\'ve ever lived outside washington state?');
+console.log('state?:' + userAnswer4);
+if (userAnswer4.toLowerCase() === 'yes' || userAnswer4.toLowerCase() === 'y') {
+  alert('correct! washington state is definitely the state for me');
+  counter++;
+} else if (userAnswer4.toLowerCase() === 'no' || userAnswer4.toLowerCase() === 'n') {
+  alert('c\'mon man I totally look pacific northwest');
+} else {
+  alert(message1);
+}
+
+var userAnswer5 = prompt('did you like this series of weird questions?');
+console.log('Like these weird questions?:' + userAnswer5);
+if (userAnswer5.toLowerCase() === 'yes' || userAnswer5.toLowerCase() === 'y') {
+  alert('alright thats pretty nice of you.');
+  counter++;
+} else if (userAnswer5.toLowerCase() === 'no' || userAnswer5.toLowerCase() === 'n') {
+  alert('it\'s ok, I wasn\'t a big fan either..');
+} else {
+  alert(message1);
+}
 
 var favNum = 9;
 var responseArr = ['you guessed too high!' , 'you guessed too low!' , 'you got it right!' , 'you took too many try\'s'];
@@ -70,6 +77,7 @@ for (var i = 0; i < 4; i++) {
   } else if (userAnswer6 == favNum) {
     alert(responseArr[2]);
     console.log(userAnswer6);
+    counter++;
     break;
   }
   if (i === 3) {
@@ -84,6 +92,7 @@ for (var i = 0; i < 6; i++) {
   if (foodsArr.includes(userAnswer7)) {
     alert('congrats you got it right! The answers were: ' + foodsArr);
     console.log(typeof userAnswer7);
+    counter++;
     break;
   } else {
     alert('guess again!');
